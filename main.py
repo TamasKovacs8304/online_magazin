400c37710bb2d890bbac68393fb50403e62a324b
altamano <tamas.kovacs83@gmail.com>
2021-02-07 11:22:00 +0000

    add elif == 18

--- main.py
+++ main.py
@@ -2,5 +2,7 @@ age = int(input('Hány éves vagy? '))
 
 if age < 18:
   print('Sajnáljuk, de ez a cikk csak 18 éven felüliek számára érhető el.')
+elif age == 18:
+  print('Pont 18? Gratulálunk, üdv a felnőttek között! Jó szórakozást a cikkhez!')
 else:
   print('Köszönjük! Elolvashatod ezt a cikket.')
