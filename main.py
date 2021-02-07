116afdfaf2a83ae504bd89ac6c28f771571aeffb
altamano <tamas.kovacs83@gmail.com>
2021-02-07 11:02:24 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+age = int(input('Hány éves vagy? '))
+
+if age < 18:
+  print('Sajnáljuk, de ez a cikk csak 18 éven felüliek számára érhető el.')
+else:
+  print('Köszönjük! Elolvashatod ezt a cikket.')
